f95bfe51695210d28ad2d17ea5c8a8ec5400331c
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-02-11 14:38:47 +0530

    success

--- 80-frontend-alb/locals.tf
+++ 80-frontend-alb/locals.tf
@@ -1,6 +1,6 @@
 locals {
     ingress_alb_sg_id = data.aws_ssm_parameter.ingress_alb_sg_id.value
-    public_subnet_ids = split(","data.aws_ssm_parameter.public_subnet_ids.value)
+    public_subnet_ids = split("," , data.aws_ssm_parameter.public_subnet_ids.value)
     ingress_alb_certificate_arn = data.aws_ssm_parameter.ingress_certificate_arn.value
     common_name_suffix = "${var.project_name}-${var.environment}"
     common_tags ={
