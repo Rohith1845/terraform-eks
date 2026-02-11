b2d7f5011fa462aff77a484387b797b740bb3b19
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-02-11 13:30:48 +0530

    success

--- 90-eks/provider.tf
+++ 90-eks/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 #  Storing state file in remote location
   backend "s3" {
-    bucket = "roboshop-dev-infra"
+    bucket = "roboshop-rohith"
     key    = "roboshop-eks-eks"
     region = "us-east-1"
     use_lockfile = true
