890e2448724d7cbe7b0622d5de77a53008b80a7a
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-02-13 10:50:59 +0530

    success

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -8,13 +8,13 @@
 
 # }
 
-resource "aws_security_group_rule" "ingress_alb_sg_id" {
+resource "aws_security_group_rule" "ingress_alb_public" {
     type = "ingress"
     security_group_id = local.ingress_alb_sg_id
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 22
+    from_port = 443
     protocol = "tcp"
-    to_port = 22
+    to_port = 443
 
 }
 
@@ -357,6 +357,16 @@ resource "aws_security_group_rule" "open_vpn_1194" {
 #     to_port = 8080
 # }
 
+resource "aws_security_group_rule" "components_vpn" {
+  for_each = local.vpn_ingress_rules
+  type              = "ingress"
+  security_group_id = each.value.sg_id
+  source_security_group_id = local.open_vpn_sg_id
+  from_port         = each.value.port
+  protocol          = "tcp"
+  to_port           = each.value.port
+}
+
 resource "aws_security_group_rule" "eks_control_plane_bastion" {
     type = "ingress"
     security_group_id = local.eks_control_plane_sg_id
@@ -375,7 +385,7 @@ resource "aws_security_group_rule" "eks_node_bastion_sg_id" {
     to_port = 22
 }
 
-resource "aws_security_group_rule" "eks_control_plane_eks_node" {
+resource "aws_security_group_rule" "eks_node_eks_control_plane" {
     type = "ingress"
     security_group_id = local.eks_control_plane_sg_id
     source_security_group_id = local.eks_node_sg_id
@@ -383,6 +393,16 @@ resource "aws_security_group_rule" "eks_control_plane_eks_node" {
     protocol = "-1"
     to_port = 0
 }
+
+resource "aws_security_group_rule" "eks_control_plane_eks_node" {
+    type = "ingress"
+    security_group_id = local.eks_node_sg_id 
+    source_security_group_id = local.eks_control_plane_sg_id
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+}
+
 
 resource "aws_security_group_rule" "eks_node_vpc" {
     type = "ingress"
